fb1545a013c94536add45d22aae341637477aa10
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-10-14 11:45:19 +0100

    Removed unesssecary dix

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,9 +4,7 @@ class Title extends React.Component {
     render(){
         return (
             <div className="title-container">
-               <div className="heading-container">
                    <h1 className="Title">Hot Dog</h1>
-               </div>
                 <h2 className="instruction">Choose your favorite</h2>
             </div>
         )
