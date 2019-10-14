942f74ef02f8a3a100f542e1b0568e4d880b8962
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-10-14 11:37:36 +0100

    Chaned rendering tree root bacl to App

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Title from './components/Title/Title'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Title />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
