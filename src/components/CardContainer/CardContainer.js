d5b7f0dd89274309239a1e66d678df33c86d81b4
AlisonYoon <politescarlett1@gmail.com>
2019-10-14 16:07:12 +0100

    commented line deleted

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -18,26 +18,9 @@ class CardContainer extends React.Component {
     }
 
     componentDidMount() {
-        let allDogs = this.getDogs()
-        // let twoRandomDogs = this.shuffleDogsArrayPickTwo(allDogs)
-        // this.setState({
-        //     dogs: twoRandomDogs
-        // })
+        this.getDogs()
     }
 
-    // getRandomDog = (dataArray) => {
-    //     return dataArray[Math.floor(Math.random() * dataArray.length)];
-    // }
-
-    // shuffleDogsArrayPickTwo = (dataArray) => {
-    //     let dog1 = this.getRandomDog(dataArray)
-    //     let dog2 = this.getRandomDog(dataArray)
-    //
-    //     while(dog1.id == dog2.id) {
-    //         dog2 = this.getRandomDog(dataArray)
-    //     }
-    //     return [dog1, dog2]
-    // }
 
     getDogs = () => {
         fetch('http://localhost:3000/dogs', {
