508b912904b607b12a4cd94b30c7ad4f0566a307
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-10-14 11:36:42 +0100

    Removed one title tags

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,11 +4,10 @@ class Title extends React.Component {
     render(){
         return (
             <div className="title-container">
-               <h1>Hot Dog</h1>
                <div className="heading-container">
-                   <h2>I am the heading</h2>
+                   <h1 className="Title">Hot Dog</h1>
                </div>
-                <h3 className="instruction">Choose your favorite</h3>
+                <h2 className="instruction">Choose your favorite</h2>
             </div>
         )
     }
