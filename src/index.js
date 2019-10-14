04ce9fd82c62d07d36ef7c8fc14f8306f5f30bcf
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-10-14 10:41:26 +0100

    Revert "Created component directory and title component files"
    This reverts commit f07d638554cac0a91124f50e18164ac00163894f.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Title from './components/Title/Title'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
