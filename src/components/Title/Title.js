0e2cab423f1f1fe811198ed001c35a5572c8898b
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-10-14 10:48:42 +0100

    Created title component which renders basic html

---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+class Title extends React.Component {
+    render(){
+        return (
+            <div>
+               <h1>I am the Title</h1>
+            </div>
+        )
+    }
+}
+
+export default Title
