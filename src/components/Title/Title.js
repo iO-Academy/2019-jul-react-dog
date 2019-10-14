00f619d666a6a8bcce6cec74a17b576a81f43614
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-10-14 10:34:56 +0100

    Added placeholder content to title component, source included in index.js and runs on npm start

---
+++ src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class title extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>I am the title</h1>
+            </div>
+        )
+    }
+}
+
+export default title
