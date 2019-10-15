6b161bccd46beb533bfbdb9d8bc52da4f8e5c6c1
Felix <felix.letheren@gmail.com>
2019-10-15 16:54:52 +0100

    Reformatted Statistics return.

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const Statistic = (props) => {
-    return (<p>
-        <span className="card-key">{props.type}: </span>
-        <span className="card-value">{props.details}</span>
-    </p>)
+    return (
+        <p>
+            <span className="card-key">{props.type}: </span>
+            <span className="card-value">{props.details}</span>
+        </p>
+    )
 }
 
 export default Statistic
