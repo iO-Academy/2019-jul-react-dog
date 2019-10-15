7c3b3a10a4a5165fd3f13191a5d345e6b7bd2d2b
Rachmann <rachmann.joubert@gmail.com>
2019-10-15 15:39:52 +0100

    Changed Card to a functional component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import Statistic from "../Statistic/Statistic"
 
-class Card extends React.Component {
-
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div className="card">
-                <Statistic type="Breed" details={this.props.name}/>
-                <Statistic type="Height" details={this.props.height}/>
-                <Statistic type="Temperament" details={this.props.temperament}/>
-            </div>
-        )
-    }
+function Card(props) {
+    return (
+        <div className="card">
+            <Statistic type="Breed" details={props.name}/>
+            <Statistic type="Height" details={props.height}/>
+            <Statistic type="Temperament" details={props.temperament}/>
+        </div>
+    )
 }
 
 export default Card
