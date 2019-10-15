5dc3eb8421842fe1b78bd6fe19cd0dcb90096c57
Liz <ejgsharp@gmail.com>
2019-10-15 14:17:14 +0100

    Created new Statistic component to resolve repetition in card component and simplify future changes

---
+++ src/components/Statistic/Statistic.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Statistic extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <p><span className="card-key">{this.props.type}: </span><span className="card-value">{this.props.details}</span></p>
+    }
+}
+
+export default Statistic
