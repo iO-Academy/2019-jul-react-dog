f9d2030de9a38056d1f713e710f9d91a5b1d844f
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-10-15 11:44:55 +0100

    fixed indentation of title

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,7 +4,7 @@ class Title extends React.Component {
     render(){
         return (
             <div className="title-container">
-                   <h1 className="Title">Hot Dog</h1>
+                <h1 className="Title">Hot Dog</h1>
                 <h2 className="instruction">Choose your favorite</h2>
             </div>
         )
