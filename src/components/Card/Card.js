a282f9730eb2cc13aa378dc5aa5f4c3085197854
Rachmann <rachmann.joubert@gmail.com>
2019-10-15 15:21:09 +0100

    Changed Card to a functional component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Card() {
+function Card(props) {
         return (
             <div className="card">
-                <p><span className="card-key">Breed : </span><span className="card-value">{this.props.name}</span></p>
-                <p><span className="card-key">Height : </span><span className="card-value">{this.props.height}</span></p>
-                <p><span className="card-key">Temperament : </span><span className="card-value">{this.props.temperament}</span></p>
+                <p><span className="card-key">Breed : </span><span className="card-value">{props.name}</span></p>
+                <p><span className="card-key">Height : </span><span className="card-value">{props.height}</span></p>
+                <p><span className="card-key">Temperament : </span><span className="card-value">{props.temperament}</span></p>
             </div>
         )
 }
