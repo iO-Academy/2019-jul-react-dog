4f0492b701f36057c14b112bace015574f7f252c
Liz <ejgsharp@gmail.com>
2019-10-15 15:31:22 +0100

    Removed constructor code

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 class Statistic extends React.Component {
-
-    constructor(props) {
-        super(props)
-    }
-
+    
     render() {
         return <p><span className="card-key">{this.props.type}: </span><span className="card-value">{this.props.details}</span></p>
     }
