4a9a3c5c4877df9ffc7b40b9fa5423b27e91b02d
AlisonYoon <politescarlett1@gmail.com>
2019-10-15 11:56:55 +0100

    Page Class component changed to functional component

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,15 +2,13 @@ import React from 'react';
 import Title from "../Title/Title";
 import CardContainer from '../CardContainer/CardContainer';
 
-class Page extends React.Component {
-    render() {
-        return (
-            <div>
-                <Title />
-                <CardContainer />
-            </div>
-        )
-    }
+function Page() {
+    return (
+        <div>
+            <Title/>
+            <CardContainer/>
+        </div>
+    )
 }
 
 export default Page
