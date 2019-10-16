c3f9013cbfdc8df07c0975110efdbefd590737d5
Rachmann <rachmann.joubert@gmail.com>
2019-10-16 11:56:33 +0100

    Fetch to update win count

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,15 +4,36 @@ import Statistic from "../Statistic/Statistic"
 class Card extends React.Component  {
     constructor(props) {
         super(props)
+
+        this.state = {
+            dogs: [],
+            winCount: 0
+        }
+    }
+
+    clickUpdateWin = () => {
+        //Matt work here
+        // let dogWhoWon = this.props.key
+        //Rachmann work here
+        fetch('http://localhost:3000/dogs', {
+            method: 'put'
+        })
+            .then(data => data.json())
+            .then(res => {
+                console.log(res)
+                const state = {...this.state, winCount: this.props.winCount}
+                this.setState(state)
+                console.log(state.dogs)
+            })
     }
 
     render() {
         return (
             <div className="card">
-                <Statistic type="Breed" details={props.name}/>
-                <Statistic type="Height" details={props.height}/>
-                <Statistic type="Temperament" details={props.temperament}/>
-                <button className="like-button"><span className="heart-icon">&#10084;</span> Like</button>
+                <Statistic type="Breed" details={this.props.name}/>
+                <Statistic type="Height" details={this.props.height}/>
+                <Statistic type="Temperament" details={this.props.temperament}/>
+                <button onClick={this.clickUpdateWin} className="like-button"><span className="heart-icon">&#10084;</span> Like</button>
             </div>
         )
     }
