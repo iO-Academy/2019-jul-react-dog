76240f3de33c2025a7fbd13bc33688b31b671c67
Felix <felix.letheren@gmail.com>
2019-10-16 15:12:18 +0100

    Removed import of Link from react-router-dom as not used.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom'
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import './index.css'
 import Page from './components/Page/Page'
 import Notfound from './components/Notfound/Notfound'
