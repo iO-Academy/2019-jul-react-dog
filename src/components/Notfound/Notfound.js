ff4503888535fcffaf0080a48609119e6f6a369a
Felix <felix.letheren@gmail.com>
2019-10-16 14:57:49 +0100

    Added Notfound.js page.

---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Title from '../Title/Title'
+
+function Notfound() {
+    return(
+       <Title titleText="404 Page not Found" instruction="Sorry about that!"/>
+    )
+}
