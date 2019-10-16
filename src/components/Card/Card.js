00710e6c088eef33f51633b8eaaa03bfa78d1868
AlisonYoon <politescarlett1@gmail.com>
2019-10-16 15:36:42 +0100

    Card class changed to functional component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import Statistic from "../Statistic/Statistic"
 
-class Card extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div className="card">
-                <Statistic type="Breed" details={this.props.name}/>
-                <Statistic type="Height" details={this.props.height}/>
-                <Statistic type="Temperament" details={this.props.temperament}/>
-                <button className="like-button" onClick={()=>{
-                    this.props.clickEvent(this.props.id)
-                }}>
-                    <span
-                    className="heart-icon">&#10084;</span> Like
-                </button>
-            </div>
-        )
-    }
+function Card(props) {
+    return (
+        <div className="card">
+            <Statistic type="Breed" details={props.name}/>
+            <Statistic type="Height" details={props.height}/>
+            <Statistic type="Temperament" details={props.temperament}/>
+            <button className="like-button" onClick={()=>{
+                props.clickEvent(props.id)
+            }}>
+                <span
+                className="heart-icon">&#10084;</span> Like
+            </button>
+        </div>
+    )
 }
 
 export default Card
