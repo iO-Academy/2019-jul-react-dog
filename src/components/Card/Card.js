deeb777b6d59adb7b924c41921b8df28fb856f78
Rachmann <rachmann.joubert@gmail.com>
2019-10-17 09:45:44 +0100

    Pulled the button tag to keep it on one line

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,11 +7,8 @@ function Card(props) {
             <Statistic type="Breed" details={props.name}/>
             <Statistic type="Height" details={props.height}/>
             <Statistic type="Temperament" details={props.temperament}/>
-            <button className="like-button" onClick={()=>{
-                props.clickEvent(props.id)
-            }}>
-                <span
-                className="heart-icon">&#10084;</span> Like
+            <button className="like-button" onClick={() => {props.clickEvent(props.id)}}>
+                <span className="heart-icon">&#10084;</span> Like
             </button>
         </div>
     )
